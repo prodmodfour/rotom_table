aec7187637c7b8c446cf3f8918fcc6282220e9c7
prodmodfour <prodmodfour@gmail.com>
2026-02-23 09:03:27 +0000

    fix: move _create-form.scss from additionalData to css array
    The _create-form.scss partial contains bare CSS rules and was
    incorrectly registered in additionalData, causing style duplication
    across all component SCSS compilations. Moved it to the css array
    for single global delivery and added @use 'variables' so its
    SCSS variable references resolve independently.

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     },
   ],
 
-  css: ['~/assets/scss/main.scss'],
+  css: ['~/assets/scss/main.scss', '~/assets/scss/_create-form.scss'],
 
   app: {
     head: {
@@ -45,7 +45,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/_variables.scss" as *; @use "~/assets/scss/_difficulty.scss" as *; @use "~/assets/scss/_pokemon-sheet.scss" as *; @use "~/assets/scss/_modal.scss" as *; @use "~/assets/scss/_sheet.scss" as *; @use "~/assets/scss/_create-form.scss" as *;'
+          additionalData: '@use "~/assets/scss/_variables.scss" as *; @use "~/assets/scss/_difficulty.scss" as *; @use "~/assets/scss/_pokemon-sheet.scss" as *; @use "~/assets/scss/_modal.scss" as *; @use "~/assets/scss/_sheet.scss" as *;'
         }
       }
     }
