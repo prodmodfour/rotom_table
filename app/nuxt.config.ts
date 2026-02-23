27ffffd63590c3ea90b25f9d781256d4af4181ef
prodmodfour <prodmodfour@gmail.com>
2026-02-23 08:00:35 +0000

    fix: extract shared create-form styles to global SCSS partial
    Scoped CSS in create.vue didn't reach nested elements inside
    QuickCreateForm.vue (Vue scoped styles only leak to the root
    element of a child component). Extract .create-form,
    .create-form__section, .create-form__actions, and .form-row
    into _create-form.scss and register it in nuxt.config.ts
    preprocessor imports so both components share the layout styles.

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -45,7 +45,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/_variables.scss" as *; @use "~/assets/scss/_difficulty.scss" as *; @use "~/assets/scss/_pokemon-sheet.scss" as *; @use "~/assets/scss/_modal.scss" as *; @use "~/assets/scss/_sheet.scss" as *;'
+          additionalData: '@use "~/assets/scss/_variables.scss" as *; @use "~/assets/scss/_difficulty.scss" as *; @use "~/assets/scss/_pokemon-sheet.scss" as *; @use "~/assets/scss/_modal.scss" as *; @use "~/assets/scss/_sheet.scss" as *; @use "~/assets/scss/_create-form.scss" as *;'
         }
       }
     }
